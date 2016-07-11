8d8655d9dfc05d9a6531c9afc0e1c357b1738ba0
Daniel Weber <dw@thedanielweber.com>
2016-07-11 19:24:24 +0000

    Timestamp Microservice working

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var app = express();
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:date', function (req, res) {
+    var unixTime = null;
+    var naturalTimeString = null;
+    
+    var datePassed = req.params.date;
+    
+    var unixDate = moment.unix(datePassed);
+    var regDate = moment(new Date(datePassed));    
+    
+    if(unixDate.isValid()) {
+        unixTime = datePassed;
+        naturalTimeString = moment.unix(datePassed).format("MMMM DD, YYYY");
+    } 
+    else if(regDate.isValid()) {
+        naturalTimeString = regDate.format("MMMM DD, YYYY").valueOf();
+        unixTime = moment(new Date(datePassed), "MMMM DD, YYYY").unix();
+    }
+    
+    // { "unix": 1450137600, "natural": "December 15, 2015" }
+    
+    res.send({ "unix": unixTime, "natural": naturalTimeString });
 });
 
 app.listen(8080, function () {
